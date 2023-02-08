5259ccac6b7a05b37b73665f2c135f45f10be5ad
shahlarbh <shahlarbh@code.edu.az>
2023-02-08 10:53:06 +0400

    last correction

--- ConsoleAppCinemaProject/Managers/CinemaPlusManager.cs
+++ ConsoleAppCinemaProject/Managers/CinemaPlusManager.cs
@@ -1,12 +1,22 @@
-﻿using Core.Models;
+﻿using ConsoleAppCinemaProject.Models;
+using Core.Models;
 using Core.Services.Contracts;
 namespace ConsoleAppCinemaProject.Managers
 {
     internal class CinemaPlusManager : ICrudService, IPrintService
     {
+        private CinemaPlus[] _cinemaPlus = new CinemaPlus[1];
+        private int _currentIndex = 0;
+
         public void Add(Entity entity)
         {
-            throw new NotImplementedException();
+            if (_currentIndex > 1)
+            {
+                Console.WriteLine("Sistemde yalniz 1 Cinema Plus movcuddur");
+                return;
+            }
+
+            _cinemaPlus[_currentIndex++] = (CinemaPlus)entity;
         }
 
         public void Delete(int id)
@@ -16,17 +26,33 @@ namespace ConsoleAppCinemaProject.Managers
 
         public Entity Get(int id)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < _cinemaPlus.Length; i++)
+            {
+                if (_cinemaPlus[i] == null)
+                    continue;
+
+                if (_cinemaPlus[i].Id == id)
+                    return _cinemaPlus[i];
+            }
+
+            Console.WriteLine("Cinema Plus not found");
+            return null;
         }
 
         public Entity[] GetAll()
         {
-            throw new NotImplementedException();
+            return _cinemaPlus;
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            foreach (var item in _cinemaPlus)
+            {
+                if (item == null)
+                    continue;
+
+                Console.WriteLine(item);
+            }
         }
 
         public void Update(int id, Entity entity)
--- ConsoleAppCinemaProject/Managers/FilmManager.cs
+++ ConsoleAppCinemaProject/Managers/FilmManager.cs
@@ -1,12 +1,22 @@
-﻿using Core.Models;
+﻿using ConsoleAppCinemaProject.Models;
+using Core.Models;
 using Core.Services.Contracts;
 namespace ConsoleAppCinemaProject.Managers
 {
     internal class FilmManager : ICrudService, IPrintService
     {
+        private Film[] _film = new Film[9];
+        private int _currentIndex = 0;
+
         public void Add(Entity entity)
         {
-            throw new NotImplementedException();
+            if (_currentIndex > 8)
+            {
+                Console.WriteLine("Sistemde yalniz 9 Film movcuddur");
+                return;
+            }
+
+            _film[_currentIndex++] = (Film)entity;
         }
 
         public void Delete(int id)
@@ -16,17 +26,33 @@ namespace ConsoleAppCinemaProject.Managers
 
         public Entity Get(int id)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < _film.Length; i++)
+            {
+                if (_film[i] == null)
+                    continue;
+
+                if (_film[i].Id == id)
+                    return _film[i];
+            }
+
+            Console.WriteLine("Film not found");
+            return null;
         }
 
         public Entity[] GetAll()
         {
-            throw new NotImplementedException();
+            return _film;
         }
 
         public void Print()
         {
-            throw new NotImplementedException();
+            foreach (var item in _film)
+            {
+                if (item == null)
+                    continue;
+
+                Console.WriteLine(item);
+            }
         }
 
         public void Update(int id, Entity entity)
